f0ca6223caf1264f2673e253e721b5efeae7934d
c19027029 <mouhamadoulaminecisse98@gmail.com>
2020-03-04 18:56:19 +0100

    fer
    Signed-off-by: c19027029 <mouhamadoulaminecisse98@gmail.com>

--- src/Verify2mot.java
+++ src/Verify2mot.java
@@ -10,27 +10,39 @@ public class Verify2mot {
             arrayword.add(word.charAt(i));
         }
         Collections.sort(arrayword);
-       System.out.println(arrayword);
         for (int i = 0 ; i < listtab.size() ; i++)
         {
             ArrayList<Character> newarrayword = (ArrayList<Character>) arrayword.clone();
             if (newarrayword.containsAll(listtab.get(i)))
             {
-               newarrayword.removeAll(listtab.get(i));
+               Verify2mot.delete(newarrayword,listtab.get(i));
                for (int j = i+1 ; j < listtab.size() ; j++)
                {
                    if (newarrayword.containsAll(listtab.get(j)))
                    {
-                       newarrayword.removeAll(listtab.get(i));
+                       Verify2mot.delete(newarrayword,listtab.get(j));
                        if (newarrayword.size()==0)
                        {
                            System.out.println(word+"est un deux mot de "+newdico.get(i)+" et de "+newdico.get(j));
                        }
+
                    }
                }
             }
         }
 
 
+    }
+    public static ArrayList<Character> delete( ArrayList<Character> mere , ArrayList<Character> fils)
+    {
+        if (mere.containsAll(fils))
+        {
+            for (int k=0;k<fils.size();k++)
+            {
+                Character local = fils.get(k);
+                mere.remove(local);
+            }
+        }
+        return mere;
     }
 }
